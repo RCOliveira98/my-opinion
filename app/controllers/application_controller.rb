ea95057d6df441af6b038db10466374c00941cf1
RCOliveira98 <rcoliveira.ti@gmail.com>
2020-06-08 18:30:36 -0300

    [app-controller] add before_action authenticate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+    # antes de tudo -- As ações com exceção da index -- confira se o usuário está autenticado
+    before_action :authenticate_user!, except: :index
 end
